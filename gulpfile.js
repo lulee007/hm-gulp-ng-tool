cac81edf3560dd1436fb0dd612168d0e0a364d29
lulee007 <lulee007@live.com>
2018-07-16 16:16:38 +0800

    移除无用文件以及代码

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,6 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
     uglify = require('gulp-uglify');
-    
-
-// var config = require('./config'),
-//     tools = require('./index'),
-    
-//     configWrap = require('./src/config-wrap');
-    
-//     configWrap.config = config;
-
-// gulp.task('default', function (cb) {
-//     console.log('gulp default start')
-//     var argv = process.argv;
-//     if (!argv || !argv[2]) {
-//         console.error('用法：gulp --port --module1,[module2,][module3,] \r\n如：gulp --8080');
-//         return;
-//     }
-//     tools.build(cb, config);
-// });
 
 gulp.task('clean-dist',function(){
     return gulp.src('./dist')
